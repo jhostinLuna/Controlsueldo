4bda8cb1bb244ef886f1b9979dbc058e5a499de2
Jhostin <jhostinluna89@gmail.com>
2021-04-13 08:26:13 +0200

    modifico la forma de capturar la fecha y creo clase Registro.

--- app/src/main/java/controlsueldo/github/io/TurnoNuevo.java
+++ app/src/main/java/controlsueldo/github/io/TurnoNuevo.java
@@ -1,8 +1,6 @@
 package controlsueldo.github.io;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
@@ -10,56 +8,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class TurnoNuevo extends AppCompatActivity implements View.OnClickListener {
     //clases donde se guardan los registros
-    Registro rEntrada;
-    Registro rSalida;
+    Registro cRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turno_nuevo);
         Button fechaEntrada,horaEntrada,fechaSalida,horaSalida;
         // Inicio los botones y los escuchadores
-        fechaEntrada = (Button) findViewById(R.id.btn_fecha_entrada);
-        horaEntrada = (Button) findViewById(R.id.btn_hora_entrada);
-        fechaSalida = (Button) findViewById(R.id.btn_fecha_salida);
-        horaSalida = (Button) findViewById(R.id.btn_hora_salida);
+        fechaEntrada = findViewById(R.id.btn_fecha_entrada);
+        horaEntrada = findViewById(R.id.btn_hora_entrada);
+        fechaSalida = findViewById(R.id.btn_fecha_salida);
+        horaSalida = findViewById(R.id.btn_hora_salida);
         fechaEntrada.setOnClickListener(this);
         horaEntrada.setOnClickListener(this);
         fechaSalida.setOnClickListener(this);
         horaSalida.setOnClickListener(this);
         // Instancia las clases
-        rEntrada = new Registro();
-        rEntrada.setEntrada(Calendar.getInstance());
-        rSalida = new Registro();
-        rSalida.setSalida(Calendar.getInstance());
+        cRegistro = new Registro();
+        cRegistro.setEntrada(Calendar.getInstance());
+        cRegistro.setSalida(Calendar.getInstance());
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
-            case R.id.btn_fecha_entrada:
-                showDatePickerDialog(v.getId());
-                break;
-            case R.id.btn_hora_entrada:
-                showTimePickerDialog(v.getId());
-                break;
-            case R.id.btn_fecha_salida:
-                showDatePickerDialog(v.getId());
-                break;
-            case R.id.btn_hora_salida:
-                showTimePickerDialog(v.getId());
-                break;
+        if (R.id.btn_fecha_entrada == v.getId()){
+            showDatePickerDialog(v.getId());
+        }else if (R.id.btn_hora_entrada == v.getId()) {
+            showTimePickerDialog(v.getId());
         }
+        else if (R.id.btn_fecha_salida == v.getId()){
+            showDatePickerDialog(v.getId());
+        }else if (R.id.btn_hora_salida == v.getId()){
+            showTimePickerDialog(v.getId());
+        }
+
     }
     private void showDatePickerDialog(int idButton) {
         DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
@@ -69,15 +60,15 @@ public class TurnoNuevo extends AppCompatActivity implements View.OnClickListene
                 Button fecha = findViewById(idButton);
                 if(idButton == R.id.btn_fecha_entrada){
                     nuevaFecha = Calendar.getInstance();
-                    nuevaFecha.set(year,month,dayOfMonth);
-                    rEntrada.setEntrada(nuevaFecha);
+                    nuevaFecha.set(year,month,dayOfMonth,cRegistro.getEntrada().get(Calendar.HOUR_OF_DAY),cRegistro.getEntrada().get(Calendar.HOUR));
+                    cRegistro.setEntrada(nuevaFecha);
                 }else {
                     nuevaFecha = GregorianCalendar.getInstance();
-                    nuevaFecha.set(year,month,dayOfMonth);
-                    rSalida.setEntrada(nuevaFecha);
+                    nuevaFecha.set(year,month,dayOfMonth,cRegistro.getSalida().get(Calendar.HOUR_OF_DAY),cRegistro.getSalida().get(Calendar.HOUR));
+                    cRegistro.setSalida(nuevaFecha);
                 }
                 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                String cadena = String.valueOf(nuevaFecha.get(Calendar.DAY_OF_MONTH))+"/"+nuevaFecha.get(Calendar.MONTH)+"/"+String.valueOf(nuevaFecha.get(Calendar.YEAR));
+                //String cadena = String.valueOf(nuevaFecha.get(Calendar.DAY_OF_MONTH))+"/"+nuevaFecha.get(Calendar.MONTH)+"/"+String.valueOf(nuevaFecha.get(Calendar.YEAR));
                 fecha.setText(sdf.format(nuevaFecha.getTime()));
             }
         });
@@ -89,11 +80,33 @@ public class TurnoNuevo extends AppCompatActivity implements View.OnClickListene
     }
     public void showTimePickerDialog(int idButton){
         TimePickerFragment newFragment = TimePickerFragment.newInstance(new TimePickerDialog.OnTimeSetListener() {
+            Calendar nuevaHora;
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                final String selectTime = hourOfDay+" : "+minute;
                 Button hora = findViewById(idButton);
-                hora.setText(selectTime);
+
+                if(idButton == R.id.btn_hora_entrada){
+                    nuevaHora = Calendar.getInstance();
+                    nuevaHora.set(
+                            cRegistro.getEntrada().get(Calendar.YEAR),
+                            cRegistro.getEntrada().get(Calendar.MONTH),
+                            cRegistro.getEntrada().get(Calendar.DAY_OF_MONTH),
+                            hourOfDay,
+                            minute);
+                    cRegistro.setEntrada(nuevaHora);
+                }else {
+                    nuevaHora = GregorianCalendar.getInstance();
+                    nuevaHora.set(
+                            cRegistro.getSalida().get(Calendar.YEAR),
+                            cRegistro.getSalida().get(Calendar.MONTH),
+                            cRegistro.getSalida().get(Calendar.DAY_OF_MONTH),
+                            hourOfDay,
+                            minute);
+                    cRegistro.setSalida(nuevaHora);
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
+                //String cadena = String.valueOf(nuevaFecha.get(Calendar.DAY_OF_MONTH))+"/"+nuevaFecha.get(Calendar.MONTH)+"/"+String.valueOf(nuevaFecha.get(Calendar.YEAR));
+                hora.setText(sdf.format(nuevaHora.getTime()));
             }
         });
         newFragment.show(getSupportFragmentManager(),"timePicker");
